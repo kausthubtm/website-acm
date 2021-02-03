4d668c0fddd412ad680f803a51a22041d8529510
Narayan Pai <narayanspai2000@gmail.com>
2021-02-03 13:12:53 +0530

    Add navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,115 +2,49 @@
 *  NAVBAR : Displays NavBar and Announcement sections                       *
 *****************************************************************************/
 
-
-
-import React from 'react';
 import '../Shared/CSS/Navbar.css'
 import '../Shared/CSS/main.css'
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom'
+import {Navbar, Nav, NavDropdown}  from 'react-bootstrap'
 
-/* imports for main nav-bar */
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Hidden from '@material-ui/core/Hidden';
+let sigs = [
+  {id: 1, name:'Sanganitra'},
+  {id:2, name:'Yantrika'},
+  {id: 3, name:'Vidyut'},
+  {id:4, name:'Kaaryavarta'},
+  {id: 5, name:'Saahitya'}
+];
 
-/* import for toggle button */
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-
-
-
-/* stuff for handelling the toggle */ 
-function HideAppBar (props) {
-
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-
+function HideAppBar () {
   return (
-    <React.Fragment>
-      {/* Navbar section */}
-      <div className="hello">
-        <div className="blah">
-
-
-          
-            <CssBaseline />
-            <AppBar position="static">
-              <Toolbar className="Navbar">
-                <Hidden smDown>
-                  <img src="/uploads/sigs/logo.png" height="60" width="60" alt="ACM logo"/>
-                  <Link to="" className="link1"></Link>
-                  <Link to="" className="nav-link">HOME</Link>
-                  <Link to="/yantra" className="nav-link">YANTRAS</Link>
-                  <Link to="/expo" className="nav-link">PROJECT EXPO</Link>
-                  <Link to="/trails" className="nav-link">TRAILS</Link>
-                  <Link to="/events" className="nav-link">EVENTS</Link>
-                  <Link to="/esp" className="nav-link">ESP</Link>
-                  <Link to="/blog" className="nav-link">BLOG</Link>
-                  <Link to="/contact" className="nav-link">CONTACT</Link>
-                </Hidden>
-                <Hidden mdUp>
-                  <img src="/uploads/sigs/logo.png" height="60" width="60" alt="ACM logo" className="toggle" />
-                  <IconButton aria-label="menu" ref={anchorRef} aria-controls={open ? 'menu-list-grow' : undefined} aria-haspopup="true" onClick={handleToggle} >
-                    <MenuIcon className="toggle_btn" style={{ fontSize: 40 }}/>
-                  </IconButton>
-                  <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-                  {({ TransitionProps, placement }) => (
-                    <Grow
-                      {...TransitionProps}
-                      style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                    >
-                      <Paper>
-                        <ClickAwayListener onClickAway={handleClose}>
-                          <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                            <MenuItem onClick={handleClose} ><Link to="" className="nav-link">HOME</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/yantra" className="nav-link">YANTRAS</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/expo" className="nav-link">PROJECT EXPO</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/trails" className="nav-link">TRAILS</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/events" className="nav-link">EVENTS</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/esp" className="nav-link">ESP</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/blog" className="nav-link">BLOG</Link></MenuItem>
-                            <MenuItem onClick={handleClose} ><Link to="/contact" className="nav-link">CONTACT</Link></MenuItem>
-                          </MenuList>
-                        </ClickAwayListener>
-                      </Paper>
-                    </Grow>
-                  )}
-                  </Popper>
-                </Hidden>
-              </Toolbar>
-            </AppBar>
-          </div>
-        </div>
-
-    </React.Fragment>
+  <div className="hello">
+    <div className="blah" style={{overflow:"visible"}}>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img src="/uploads/sigs/logo.png" height="60" width="60" alt="ACM logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav activeKey={useLocation().pathname} className="mr-auto">
+              <Nav.Link href="/">HOME</Nav.Link>
+              <NavDropdown title="YANTRAS" id="collapsible-nav-dropdown">
+                {
+                  sigs.map(sig=>(
+                    <NavDropdown.Item href={`/sigs/${sig.id}`}>{sig.name.toUpperCase()}</NavDropdown.Item>
+                  ))
+                }
+              </NavDropdown>
+              <Nav.Link href="/expo">PROJECT EXPO</Nav.Link>
+              <Nav.Link href="https://nitk.acm.org/trails.html">TRAILS</Nav.Link>
+              <Nav.Link href="/events">EVENTS</Nav.Link>
+              <Nav.Link href="/esp">ESP</Nav.Link>
+              <Nav.Link href="https://nitk.acm.org/blog/">BLOG</Nav.Link>
+              <Nav.Link href="/contact">CONTACT US</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </div>
   );
 }
 
