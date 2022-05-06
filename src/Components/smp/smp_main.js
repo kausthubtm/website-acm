48ff335b79274a7c80cf767b4cb83808a6527f1e
Amogh Umesh <amoghumesh02@gmail.com>
2022-05-06 19:40:44 +0530

    added correct link

--- src/Components/smp/smp_main.js
+++ src/Components/smp/smp_main.js
@@ -100,7 +100,7 @@ class Smp_main extends Component{
                 <h1 className="banner_text1">Summer Mentorship Program</h1>
               </header>
               <center style = {{marginTop:'50px'}} >
-              <Button variant="contained" color="primary" className="register-now-button" onClick={()=>{window.location.href = 'https://forms.gle/eb29jZZ2SveSCJva6'}} >Register Here !</Button>
+              <Button variant="contained" color="primary" className="register-now-button" onClick={()=>{window.location.href = 'https://forms.gle/TUKYZuE8xVR6wJju6'}} >Register Here !</Button>
                 
               </center>
             </div>
